8ac030544b596d4636b8c96a7e5dcc2c59adfbc0
AliTurpalov <alishkatur6@gmail.com>
2024-05-10 13:11:09 +0300

    Update ExtendedFunctionalityTest.java

--- src/test/java/ExtendedFunctionalityTest.java
+++ src/test/java/ExtendedFunctionalityTest.java
@@ -7,20 +7,15 @@ public class ExtendedFunctionalityTest {
 
     @Test
     public void testRandomArrayOperations() {
-        // Генерация массива из 15 случайных чисел от 1 до 10
         List<Integer> numbers = generateRandomNumbers(15, 1, 10);
-        
-        // Вывод сгенерированного массива
-        System.out.println("Сгенерированный массив: " + numbers);
+        System.out.println("Massiv: " + numbers);
 
-        // Выполнение функций и вывод результатов
-        System.out.println("Минимальное значение: " + StatisticsCalculator._min(numbers));
-        System.out.println("Максимальное значение: " + StatisticsCalculator._max(numbers));
-        System.out.println("Сумма всех чисел: " + StatisticsCalculator._sum(numbers));
-        System.out.println("Произведение всех чисел: " + StatisticsCalculator._mult(numbers));
+        System.out.println("Min: " + StatisticsCalculator._min(numbers));
+        System.out.println("Max: " + StatisticsCalculator._max(numbers));
+        System.out.println("Sum: " + StatisticsCalculator._sum(numbers));
+        System.out.println("Proizvedenie: " + StatisticsCalculator._mult(numbers));
     }
 
-    // Метод для генерации списка случайных чисел
     private List<Integer> generateRandomNumbers(int count, int min, int max) {
         List<Integer> numbers = new ArrayList<>();
         Random random = new Random();
